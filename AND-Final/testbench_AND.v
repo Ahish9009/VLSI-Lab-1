5aa3e8040fa224edc850ec1865fae3c93bc732b9
Ahish9009 <ahishjayant9009@gmail.com>
2019-09-04 19:34:54 +0530

    AND-Final initialized

---
+++ AND-Final/testbench_AND.v
@@ -0,0 +1,31 @@
+
+module andGate_testbench;
+wire testY;
+reg testA, testB;
+
+andGate gate1(testA, testB, testY);
+
+initial
+begin
+
+	$monitor(testA, testB, testY);
+
+	testA = 1'b0;
+	testB = 1'b0;
+
+	#5 //5ns delay
+	testA = 1'b0;
+	testB = 1'b1;
+
+	#5 //5ns delay
+	testA = 1'b1;
+	testB = 1'b0;
+
+	#5 //5ns delay
+	testA = 1'b1;
+	testB = 1'b1;
+	
+	#5;
+
+end
+endmodule
