0aad6795cca1c621a6f0b60fdc2422778513ac35
Ahish9009 <ahishjayant9009@gmail.com>
2019-09-04 09:37:39 +0530

    Update f1

--- F1/f1.v
+++ F1/f1.v
@@ -2,5 +2,10 @@
 module f1(a, b, c, d, op);
 input a, b, c, d;
 output op;
-assign op = (a&b)|(c&d);
+//assign op = (a&b)|(c&d);
+wire op;
+and op1 (x, a, b);
+and op2 (y, c, d);
+or op3 (op, x, y);
+
 endmodule
